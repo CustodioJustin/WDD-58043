aec8a0eea09c841fea1df04d439eff6ca7e35efd
CustodioJustin <78063106+CustodioJustin@users.noreply.github.com>
2022-09-30 09:30:38 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,23 @@
+<html>
+	<head><title>Exercise1</title></head>
+	<body bgcolor = lightblue>
+		<center>
+		<h2>Exercise 1</h2>
+		<h3>Adamson University</h3>	
+		<?php
+			$fullname = "Custodio, Justin Chyle C.";
+			$collegeprogram = "Computer Engineering";
+			$yearnsection = "3rd Year - 58043";
+			
+			echo $fullname;
+			echo "<br>";
+			echo $collegeprogram;
+			echo "<br>";
+			echo $yearnsection;
+			echo "<br>";
+			echo "<br>";
+			echo "A filler text or string";
+			
+		?></center>
+	</body>
+</html>
