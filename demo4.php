26cccda2ea51848505fa2dd1e30bf4b49bbae817
CustodioJustin <78063106+CustodioJustin@users.noreply.github.com>
2022-10-21 09:20:45 +0800

    Add files via upload

---
+++ demo4.php
@@ -0,0 +1,26 @@
+<html>
+	<head><title>Looping Statement</title></head>
+	<body>
+		
+		<?php 
+		
+		//$fuel = 10;
+		//do {
+		//echo "There's enoug fuel <br>";
+		//} while (--$fuel >= 0);
+		
+		//for ($fuel = 10; $fuel >= 1;$fuel--){
+		//	echo "There's enoug fuel <br>";
+		//}
+		
+		$x = 10;
+		do {
+			if ($x < 10 && $x >= 3) {
+				echo "$x <br>";
+				}
+		}	while(--$x >= 0)
+		
+		?>
+		
+	</body>
+</html>
